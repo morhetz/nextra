be23dff1c18226bfa787488224c2fb84144973de
Pavel Pertsev <morhetz@gmail.com>
2021-04-12 20:09:19 +0300

    cleanup

--- examples/docs/next.config.js
+++ examples/docs/next.config.js
@@ -1,7 +1,10 @@
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+const withNextra = require('@dextcloud/nextra')(
+  '@dextcloud/nextra-theme-docs',
+  './theme.config.js',
+)
 module.exports = withNextra({
   i18n: {
     locales: ['en', 'ar', 'zh'],
-    defaultLocale: 'en'
-  }
+    defaultLocale: 'en',
+  },
 })
--- packages/nextra-theme-blog/src/utils/get-title.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function getTitle(children) {
-  const nodes = React.Children.toArray(children)
-  const titleEl = nodes.find(child => child.props.mdxType === 'h1')
-  return [titleEl || null, nodes.filter(node => node !== titleEl)]
-}
